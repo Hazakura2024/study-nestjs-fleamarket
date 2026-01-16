a4cd07c292fdd00cb9bc64129ad471a8e9bb2881
Hazakura2024 <ytr240.jp@gmail.com>
2026-01-16 13:35:56 +0900

    feat: corsの設定を追加

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //NOTE: すべてのオリジンからの全てのリクエストを許可
+  app.enableCors();
+  // //NOTE: 特定の場合のみを許可
+  // app.enableCors({
+  //   origin: ['http://example.com:1234'],
+  //   methods: ['GET', 'POST'],
+  // });
   //NOTE: グローバルにバリデーションを適用
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
