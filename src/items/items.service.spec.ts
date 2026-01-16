fd19e081d90161d09887f6f8fc5a8d44e5690e15
Hazakura2024 <ytr240.jp@gmail.com>
2026-01-16 16:47:58 +0900

    test: findByIdのtestを作成

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,10 +1,13 @@
 import { Test } from '@nestjs/testing'
 import { ItemsService } from './items.service'
 import { PrismaService } from '../prisma/prisma.service';
+import { Item, ItemStatus } from '@prisma/client';
+import { NotFoundException } from '@nestjs/common';
 
 const mockPrismaService = {
   item: {
     findMany: jest.fn(),
+    findUnique: jest.fn(),
   },
 };
 
@@ -32,6 +35,35 @@ describe('ItemsServiceTest', () => {
       const expected = [];
       const result = await itemsService.findAll();
       expect(result).toEqual(expected);
+    });
+  });
+
+  describe('findById', () => {
+    it('正常系', async () => {
+      const item: Item = {
+        id: 'test-id1',
+        name: 'test-item1',
+        price: 100,
+        description: '',
+        status: ItemStatus.ON_SALE,
+        createdAt: new Date('2026-02-01'),
+        updatedAt: new Date('2026-02-01'),
+        userId: 'test-user1',
+      };
+      (prismaService.item.findUnique as jest.Mock).mockResolvedValue(item);
+
+      const result = await itemsService.findById('test-id1');
+      expect(result).toEqual(item);
+      expect(prismaService.item.findUnique).toHaveBeenCalledWith({
+        where: { id: 'test-id1' },
+      });
+    });
+    it('異常系: 商品が存在しない', async () => {
+      (prismaService.item.findUnique as jest.Mock).mockResolvedValue(null);
+
+      await expect(itemsService.findById('test-id1')).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
